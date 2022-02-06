4b1e700ffc286626f5f2a726f1e0aa5d3ea7e2d4
fescarvalho <fernando_scarvalho@hotmail.com>
2022-02-06 14:12:08 -0300

    parametros e perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//links header
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,38 @@ function ativarLink(link) {
   }
 }
 links.forEach(ativarLink);
+
+//selecao orcamento ao clicar
+const params = new URLSearchParams(location.search);
+
+function activeParam(param) {
+  const element = document.getElementById(param);
+
+  if (param) {
+    element.checked = true;
+  }
+  console.log(param);
+}
+params.forEach(activeParam);
+
+//perguntas frequentes para
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function mostrarResposta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativo");
+  const ativa = resposta.classList.contains("ativo");
+
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function pegarPergunta(pergunta) {
+  pergunta.addEventListener("click", mostrarResposta);
+}
+
+perguntas.forEach(pegarPergunta);
+
+console.log(perguntas);
